404a60dab36d70ba142b6742521c7eb71b23f470
master <master@SMLAB310PC14_23>
2023-06-12 19:22:09 -0500

    Agregar archivos de proyecto.

---
+++ aQuincheS3/Page1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace aQuincheS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string datoUno = txtDatoUno.Text;
+            int datoDos = Convert.ToInt32(txtDatoDos.Text);
+            Navigation.PushAsync(new Page2(datoUno, datoDos));
+        }
+    }
+}
---
+++ aQuincheS3/Page2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace aQuincheS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string datoUno, int datoDos)
+        {
+            InitializeComponent();
+            lblDatoUno.Text = "DATO UNO:" + datoUno;
+            lblDatoDos.Text = "DATO DOS:" + datoDos;
+
+        }
+
+        private void btnAbrirP_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
